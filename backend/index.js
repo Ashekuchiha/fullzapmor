1e89af9f8866ab4290c8b9aaf153020806ac2b3d
Ashekuchiha <ashakulislam0000@gmail.com>
2024-10-12 22:32:05 +0600

    delet index

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-
-const PORT = process.env.PORT || 5000;
-
-// Start the server and listen on a specified port
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
